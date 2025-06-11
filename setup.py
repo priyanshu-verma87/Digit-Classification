78b237b7359d9b3f942e441ef6ac69b03f39a633
Priyanshu Verma <165484223+priyanshu-verma87@users.noreply.github.com>
2025-06-11 20:35:34 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-setup(
-    name="digit-classifier",
-    python_requires=">=3.10,<3.11",  # Hard lock to Python 3.10
-    install_requires=["tensorflow==2.12.0", "streamlit"],
-)
